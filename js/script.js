0ce5b1a368fc4b609b37882d346ef756ec4deb6d
Geershati Saxena <geershatisaxena110@gmail.com>
2026-02-04 20:28:33 +0530

    .

--- js/script.js
+++ js/script.js
@@ -1512,17 +1512,6 @@ const songs = [
         src:"assets/songs/Mere Haath Mein Fanaa 128 Kbps.mp3",
         cover:"assets/images/mere hath me.jpg"
     },
-
-
-
-
-
-
-
-
-
-
-
 ];
 
 // DOM Elements
